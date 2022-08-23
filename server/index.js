f647b92fdf1a11da65b4ae9b9ca35f070782237d
dunglv2 <dunglv2@rikkeisoft.com>
2022-08-23 11:18:46 +0700

    cors config

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 // import libs
 const express = require('express')
 const connectDB = require('./common/connectDB')
+const cors = require('cors')
 
 // Connect databse
 connectDB()
@@ -11,10 +12,26 @@ connectDB()
 // run
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+// socket.io
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('user connected!')
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+})
 
 // Route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
 app.use('/api/account', require('./routes/account'))
 app.use('/api/room', require('./routes/room'))
 app.use('/api/message', require('./routes/message'))
 
-app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
+http.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
